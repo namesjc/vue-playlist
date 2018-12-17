c24adea578e8c9d88883b63ded3026922ca9124a
CJE5SZH <adiachan@outlook.com>
2018-12-17 21:34:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <app-header @titleChanged="updateTitle($event)" :title="title"></app-header>
+    <users :users="usersAarry"></users>
+    <users :users="usersAarry"></users>
+    <app-footer :title="title"></app-footer>
+  </div>
+</template>
+
+<script>
+
+import Users from './components/Users'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      usersAarry: [
+        {name: "Echo", position: "Manager", show: false},
+        {name: "Marco", position: "Web developer", show: false},
+        {name: "Steve", position: "Captain America", show: false},
+        {name: "Tony", position: "Icon Man", show: false},
+        {name: "Peter", position: "Sipder Man", show: false},
+        {name: "Bruce", position: "Hulk", show: false}
+      ],
+  title: "Passing a value(number string boolean)"         
+    }   
+  },
+  methods: {
+    updateTitle(changedTitle) {
+      this.title = changedTitle;
+    }
+  },
+  components: {
+    "users": Users,
+    "app-header": Header,
+    "app-footer": Footer
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: purple;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+    <header>
+        <h1 @click="changeTitle">{{ message }}-{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'app-header',
+  props: {
+      title: {
+          type: String
+      }
+  },
+  data () {
+    return {
+      message: "Vue.js Demo"
+    }
+  },
+  methods: {
+      changeTitle() {
+        //   this.title = "changed";
+        this.$emit("titleChanged", "child passes value to parent");
+      }
+  },
+  beforeCreate() {
+      alert("executed function before vue instance initialized");
+  },
+  created() {
+      alert("vue instance initialize completed, but the page doesn't show");
+  },
+  beforeMount() {
+      alert("before vue instance mounted, the page still doesn't show, but vDOM already configured");
+  },
+  mounted() {
+      alert("vue instance mounted, the page shows after the method be executed")
+  },
+  beforeUpdate() {
+      alert("before vue instance update, the page still not update, but vDOM already configured")
+  },
+  updated() {
+      alert("vue instance update, the updated page show after the method be executed")
+  },
+  beforeDestroy() {
+      alert("before vue instance destory")
+  },
+  destroyed() {
+      alert("vue instance be destoryed")
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    header {
+        background: lightgreen;
+        padding: 10px;
+    }
+
+    h1 {
+       color: #222;
+       text-align: center; 
+    }
+</style>
